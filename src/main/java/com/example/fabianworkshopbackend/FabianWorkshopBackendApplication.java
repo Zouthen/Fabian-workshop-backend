8753fe191d069c53ac52dd8e111a340f7621904f
Zouthen <mich5n69@stud.kea.dk>
2023-05-03 10:18:34 +0200

    Initial commit

---
+++ src/main/java/com/example/fabianworkshopbackend/FabianWorkshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fabianworkshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabianWorkshopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabianWorkshopBackendApplication.class, args);
+    }
+
+}
